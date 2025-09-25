94cc5cfaa49c19ba0ba649c48c96bf07adb118ed
Kyrylo Popov <popovkirill070320@gmail.com>
2025-09-25 13:06:54 +0300

    Коментарі до файлу PersonDbContext

--- TestApplicationPopovKyrylo/Models/PersonDbContext.cs
+++ TestApplicationPopovKyrylo/Models/PersonDbContext.cs
@@ -2,10 +2,13 @@
 
 namespace TestApplicationPopovKyrylo.Models
 {
-    public class PersonDbContext : DbContext
+    public class PersonDbContext : DbContext // підключення до БД, робота з транзакціями, CRUD-операції
     {
-        public DbSet<Person> people { get; set; }
+        public DbSet<Person> people { get; set; } // оголошення таблиці (колекції сутностей)
+        // для роботи із даними як із колекцією (а не таблицею і рядком SQL)
 
-        public PersonDbContext(DbContextOptions<PersonDbContext> options) : base(options){}
+        public PersonDbContext(DbContextOptions<PersonDbContext> options) : base(options) { }
+        // підключення до відповідної БД, врахування додаткового функціоналу (lazy loading, логування)
+        // як поводитись із транзакціями та міграціями
     }
 }
